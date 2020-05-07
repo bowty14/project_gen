d1456a829e1789bce7aecaac13d580f68ef5ec78
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-07 14:58:19 -0700

    add functionality that creates all the folders and files needed for a ruby project folder.

---
+++ project_gen_script.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+require ('fileutils')
+
+class Project 
+  def initialize (project_name)
+    @project_name = project_name
+  end
+  def project_gen 
+    FileUtils.mkdir_p "../#{@project_name}/lib"
+    FileUtils.mkdir_p "../#{@project_name}/spec"
+    FileUtils.touch "../#{@project_name}/lib/#{@project_name}.rb"
+    FileUtils.touch "../#{@project_name}/spec/#{@project_name}_spec.rb"
+    File.open("../#{@project_name}/Gemfile", 'a') {|f| f.write(" 
+      source 'https://rubygems.org' 
+      gem 'pry' 
+      gem 'rspec' ") }
+    File.open("../#{@project_name}/lib/#{@project_name}.rb", 'a') {|f| f.write("
+      require ('pry')
+
+      class
+
+      end") }
+    File.open("../#{@project_name}/spec/#{@project_name}_spec.rb", 'a') {|f| f.write("
+      require ('pry')
+      require ('rspec')") }
+  end
+end
+
+
+puts "What would you like your Ruby file to be called?"
+file_name = gets.chomp
+project = Project.new(file_name)
+puts (project.project_gen)
+
+
